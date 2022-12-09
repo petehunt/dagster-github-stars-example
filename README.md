bcc9cd2daa60ecaa05220722367c2feb1e9c4ff6
Pete Hunt <floydophone@gmail.com>
2022-12-09 03:44:18 +0000

    Migrate example to use modern best practices

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+This is the repo corresponding to the [Dagster crash course](https://dagster.io/blog/dagster-crash-course-oct-2022).
+
 [![Open in Gitpod](https://gitpod.io/button/open-in-gitpod.svg)](https://gitpod.io/#https://github.com/petehunt/dagster-github-stars-example)
--- my-dagster-project/my_dagster_project/__init__.py
+++ my-dagster-project/my_dagster_project/__init__.py
@@ -1,1 +1,20 @@
-from .repository import my_dagster_project
+from dagster import (
+    load_assets_from_package_module,
+    Definitions,
+    define_asset_job,
+    ScheduleDefinition,
+)
+from my_dagster_project import assets
+import os
+from github import Github
+
+defs = Definitions(
+    assets=load_assets_from_package_module(assets),
+    schedules=[
+        ScheduleDefinition(
+            job=define_asset_job(name="daily_refresh", selection="*"),
+            cron_schedule="@daily",
+        )
+    ],
+    resources={"github_api": Github(os.environ["GITHUB_ACCESS_TOKEN"])},
+)
